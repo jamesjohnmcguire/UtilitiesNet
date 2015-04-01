06c76b0bd0f7b0f4138dc9ae186b02a8791494dd
James John McGuire <jamesjohnmcguire@gmail.com>
2015-04-01 14:07:44 +0000

    Added ToCamelCase and ToPascalCase.

--- StringExtender.cs
+++ StringExtender.cs
@@ -31,12 +31,59 @@ namespace Zenware.Common.UtilsNet.Extensions
 			return false;
 		}
 
+		// Convert the string to camel case.
+		public static string ToCamelCase(this string the_string)
+		{
+			// If there are 0 or 1 characters, just return the string.
+			if (the_string == null || the_string.Length < 2)
+				return the_string;
+
+			// Split the string into words.
+			string[] words = the_string.Split(
+				new char[] { },
+				StringSplitOptions.RemoveEmptyEntries);
+
+			// Combine the words.
+			string result = words[0].ToLower();
+			for (int i = 1; i < words.Length; i++)
+			{
+				result +=
+					words[i].Substring(0, 1).ToUpper() +
+					words[i].Substring(1);
+			}
+
+			return result;
+		}
+	
 		public static string ToDigitsOnly(this string input)
 		{
 			Regex digitsOnly = new Regex(@"[^\d]");
 			return digitsOnly.Replace(input, "");
 		}
 
+		// Convert the string to Pascal case.
+		public static string ToPascalCase(this string the_string)
+		{
+			// If there are 0 or 1 characters, just return the string.
+			if (the_string == null) return the_string;
+			if (the_string.Length < 2) return the_string.ToUpper();
+
+			// Split the string into words.
+			string[] words = the_string.Split(
+				new char[] { },
+				StringSplitOptions.RemoveEmptyEntries);
+
+			// Combine the words.
+			string result = "";
+			foreach (string word in words)
+			{
+				result +=
+					word.Substring(0, 1).ToUpper() +
+					word.Substring(1);
+			}
+
+			return result;
+		}
 		public static string ToProperCase(this string unformattedText)
 		{
 			string formattedText = null;
