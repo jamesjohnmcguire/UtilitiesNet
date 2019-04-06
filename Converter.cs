e4501c077b7bdd8e54434c3e75af91c9891e4571
JamesMc <jamesjohnmcguire@gmail.com>
2019-04-06 14:49:48 +0900

    Added in functionality from other external modules.

---
+++ Converter.cs
@@ -0,0 +1,24 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// $Id$
+// <copyright file="Converter.cs" company="James John McGuire">
+// Copyright © 2006 - 2019 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalZenWorks.Common.Utilities
+{
+	public static class Converter
+	{
+		public static byte[] StringToByteArray(
+			string original)
+		{
+			ASCIIEncoding encoding = new ASCIIEncoding();
+			byte[] byteArrary = encoding.GetBytes(original);
+			return byteArrary;
+		}
+	}
+}
