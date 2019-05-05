d8ca2cbb212ee5cb7b0c12ff3e693a337bee9a5d
JamesMc <jamesjohnmcguire@gmail.com>
2019-05-05 22:37:30 +0900

    Moved pluralation service to here.

---
+++ PluralizationServiceInstance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using PluralizationService;
+using PluralizationService.Core;
+using PluralizationService.English;
+
+namespace DigitalZenWorks.Common.Utilities
+{
+	public static class PluralizationServiceInstance
+	{
+		private static readonly IPluralizationApi Api = BuildApi();
+		private static readonly CultureInfo CultureInfo =
+			new CultureInfo("en-US");
+
+		static IPluralizationApi BuildApi()
+		{
+			PluralizationApiBuilder builder = new PluralizationApiBuilder();
+			builder.AddEnglishProvider();
+
+			IPluralizationApi api = builder.Build();
+
+			return api;
+		}
+
+		public static string Pluralize(string name)
+		{
+			return Api.Pluralize(name, CultureInfo) ?? name;
+		}
+
+		public static string Singularize(string name)
+		{
+			return Api.Singularize(name, CultureInfo) ?? name;
+		}
+	}
+}
