f315769557d0ddf1f3f26c50825f95065b724b78
JamesMc <jamesjohnmcguire@gmail.com>
2021-06-04 21:02:56 +0900

    Added VersionFileType enum.

---
+++ VersionFileType.cs
@@ -0,0 +1,29 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="VersionFileType.cs" company="James John McGuire">
+// Copyright © 2006 - 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.Common.Utilities
+{
+	/// <summary>
+	/// Version file types.
+	/// </summary>
+	public enum VersionFileType
+	{
+		/// <summary>
+		/// A generic version file.
+		/// </summary>
+		Generic,
+
+		/// <summary>
+		/// An assembly info file.
+		/// </summary>
+		AssemblyInfo,
+
+		/// <summary>
+		/// A c sharp project file.
+		/// </summary>
+		CsProj
+	}
+}
