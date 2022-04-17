9776ced39469905dda08b724ee35fd162866e34d
James John McGuire <jamesjohnmcguire@gmail.com>
2022-04-17 19:24:02 +0900

    Add additional byte related functionality
    From other projects.

--- BitBytes.cs
+++ BitBytes.cs
@@ -16,6 +16,82 @@ namespace DigitalZenWorks.Common.Utilities
 	public static class BitBytes
 	{
 		/// <summary>
+		/// Copies the array if existent.
+		/// </summary>
+		/// <param name="source">The source buffer.</param>
+		/// <param name="destination">The destination buffer.</param>
+		/// <param name="index">The destination index.</param>
+		/// <returns>The updated index.</returns>
+		public static long ArrayCopyConditional(
+			byte[] source, ref byte[] destination, long index)
+		{
+			if (source != null)
+			{
+				Array.Copy(
+					source,
+					0,
+					destination,
+					index,
+					source.LongLength);
+				index += source.LongLength;
+			}
+
+			return index;
+		}
+
+		/// <summary>
+		/// Copy an integer into a byte array.
+		/// </summary>
+		/// <param name="bytes">The byte array.</param>
+		/// <param name="index">The index to copy at.</param>
+		/// <param name="value">The integer value.</param>
+		/// <returns>The updated byte array.</returns>
+		public static byte[] CopyIntToByteArray(
+			byte[] bytes, long index, int value)
+		{
+			byte byteValue1 = (byte)value;
+			byte byteValue2 = (byte)(value >> 8);
+			byte byteValue3 = (byte)(value >> 0x10);
+			byte byteValue4 = (byte)(value >> 0x18);
+
+			if (bytes != null)
+			{
+				bytes[index] = byteValue1;
+				index++;
+				bytes[index] = byteValue2;
+				index++;
+				bytes[index] = byteValue3;
+				index++;
+				bytes[index] = byteValue4;
+			}
+
+			return bytes;
+		}
+
+		/// <summary>
+		/// Copy an unsigned short into a byte array.
+		/// </summary>
+		/// <param name="bytes">The byte array.</param>
+		/// <param name="index">The index to copy at.</param>
+		/// <param name="value">The unsigned short value.</param>
+		/// <returns>The updated byte array.</returns>
+		public static byte[] CopyUshortToByteArray(
+			byte[] bytes, long index, ushort value)
+		{
+			byte byteValue1 = (byte)value;
+			byte byteValue2 = (byte)(value >> 8);
+
+			if (bytes != null)
+			{
+				bytes[index] = byteValue1;
+				index++;
+				bytes[index] = byteValue2;
+			}
+
+			return bytes;
+		}
+
+		/// <summary>
 		/// Finds an item in a byte array.
 		/// </summary>
 		/// <param name="haystack">The byte array haystack.</param>
@@ -54,6 +130,29 @@ namespace DigitalZenWorks.Common.Utilities
 			}
 
 			return foundPosition;
+		}
+
+		/// <summary>
+		/// Get bit value, as boolean.
+		/// </summary>
+		/// <param name="rawValue">The byte with the bit to check.</param>
+		/// <param name="bitNumber">The 0 based index of the bit
+		/// to retrieve.</param>
+		/// <returns>The bit value, as boolean.</returns>
+		public static bool GetBit(byte rawValue, byte bitNumber)
+		{
+			bool bitValue = false;
+
+			// 0 based
+			byte shifter2 = (byte)(1 << bitNumber);
+			byte bit = (byte)(rawValue & shifter2);
+
+			if (bit != 0)
+			{
+				bitValue = true;
+			}
+
+			return bitValue;
 		}
 
 		/// <summary>
@@ -128,6 +227,112 @@ namespace DigitalZenWorks.Common.Utilities
 			Encoding encoding = Encoding.UTF8;
 			byte[] byteArrary = encoding.GetBytes(original);
 			return byteArrary;
+		}
+
+		/// <summary>
+		/// To integer method.
+		/// </summary>
+		/// <param name="bytes">The source bytes.</param>
+		/// <param name="index">The index with in the bytes to copy.</param>
+		/// <returns>An integer of the bytes values.</returns>
+		public static uint ToInteger(byte[] bytes, uint index)
+		{
+			uint result = ToIntegerLimit(bytes, index, 4);
+
+			return result;
+		}
+
+		/// <summary>
+		/// To integer limit method.
+		/// </summary>
+		/// <param name="bytes">The source bytes.</param>
+		/// <param name="index">The index with in the bytes to copy.</param>
+		/// <param name="limit">The amount of bytes to copy.</param>
+		/// <returns>An integer of the bytes values.</returns>
+		public static uint ToIntegerLimit(byte[] bytes, uint index, int limit)
+		{
+			uint result;
+
+			// The converter still needs 4 bytes to act on.
+			byte[] testBytes = new byte[4];
+			Array.Copy(bytes, index, testBytes, 0, limit);
+
+			// Dbx files are apprentely stored as little endian.
+			if (BitConverter.IsLittleEndian == false)
+			{
+				Array.Reverse(testBytes);
+			}
+
+			result = BitConverter.ToUInt32(testBytes, 0);
+
+			return result;
+		}
+
+		/// <summary>
+		/// To integer array method.
+		/// </summary>
+		/// <param name="bytes">The source bytes.</param>
+		/// <returns>An integer array of the bytes values.</returns>
+		public static uint[] ToIntegerArray(byte[] bytes)
+		{
+			uint[] integerArray = null;
+
+			if (bytes != null)
+			{
+				int size = bytes.Length / sizeof(uint);
+				integerArray = new uint[size];
+				Buffer.BlockCopy(bytes, 0, integerArray, 0, bytes.Length);
+			}
+
+			return integerArray;
+		}
+
+		/// <summary>
+		/// To integer method.
+		/// </summary>
+		/// <param name="bytes">The source bytes.</param>
+		/// <param name="index">The index with in the bytes to copy.</param>
+		/// <returns>An integer of the bytes values.</returns>
+		public static ulong ToLong(byte[] bytes, uint index)
+		{
+			ulong result;
+
+			// The converter still needs 8 bytes to act on.
+			byte[] testBytes = new byte[8];
+			Array.Copy(bytes, index, testBytes, 0, 8);
+
+			// Dbx files are apprentely stored as little endian.
+			if (BitConverter.IsLittleEndian == false)
+			{
+				Array.Reverse(testBytes);
+			}
+
+			result = BitConverter.ToUInt64(testBytes, 0);
+
+			return result;
+		}
+
+		/// <summary>
+		/// To short method.
+		/// </summary>
+		/// <param name="bytes">The source bytes.</param>
+		/// <param name="index">The index with in the bytes to copy.</param>
+		/// <returns>An integer of the bytes values.</returns>
+		public static ushort ToShort(byte[] bytes, int index)
+		{
+			ushort result;
+			byte[] testBytes = new byte[2];
+			Array.Copy(bytes, index, testBytes, 0, 2);
+
+			// Dbx files are apprentely stored as little endian.
+			if (BitConverter.IsLittleEndian == false)
+			{
+				Array.Reverse(testBytes);
+			}
+
+			result = BitConverter.ToUInt16(testBytes, 0);
+
+			return result;
 		}
 	}
 }
