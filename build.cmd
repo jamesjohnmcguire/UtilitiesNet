9510c414f99abb239c7cd127c5bcfe8498a184ad
James John McGuire <jamesjohnmcguire@gmail.com>
2022-04-29 21:25:55 +0900

    Ensure publishing with 'Any CPU'

--- build.cmd
+++ build.cmd
@@ -3,8 +3,8 @@ REM %2 - API key
 
 CD %~dp0
 
-msbuild -property:Configuration=Release -restore UtilitiesNET.csproj
-msbuild -property:Configuration=Release;OutputPath=Bin\Nuget UtilitiesNET.csproj
+msbuild -property:Configuration=Release;OutputPath=Bin\Release\Nuget;Platform="Any CPU" -restore UtilitiesNET.csproj
+msbuild -property:Configuration=Release;OutputPath=Bin\Release\Nuget;Platform="Any CPU" UtilitiesNET.csproj
 
-CD Bin\Nuget
+CD Bin\Release\Nuget
 dotnet nuget push DigitalZenWorks.Common.Utilities.%1.nupkg --api-key %2 --source https://api.nuget.org/v3/index.json
