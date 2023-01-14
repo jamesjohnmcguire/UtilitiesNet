b72d952a7d5afc51bbed7f0d9990c8a567977bd3
James John McGuire <jamesjohnmcguire@gmail.com>
2023-01-15 00:18:37 +0900

    Add Parameter Checking for Build

--- build.cmd
+++ build.cmd
@@ -3,8 +3,30 @@ REM %2 - API key
 
 CD %~dp0
 
+IF "%1"=="publish" GOTO publish
+
+:default
+dotnet build
+
+GOTO end
+
+:publish
+
+if "%~2"=="" GOTO error1
+if "%~3"=="" GOTO error2
+
 msbuild -property:Configuration=Release;OutputPath=Bin\Release\Nuget;Platform="Any CPU" -restore UtilitiesNET.csproj
-msbuild -property:Configuration=Release;OutputPath=Bin\Release\Nuget;Platform="Any CPU" UtilitiesNET.csproj
 
 CD Bin\Release\Nuget
-dotnet nuget push DigitalZenWorks.Common.Utilities.%1.nupkg --api-key %2 --source https://api.nuget.org/v3/index.json
+dotnet nuget push DigitalZenWorks.Common.Utilities.%2.nupkg --api-key %3 --source https://api.nuget.org/v3/index.json
+
+cd ..\..\..
+
+:error1
+ECHO No version tag specified
+GOTO end
+
+:error2
+ECHO No API key specified
+
+:end
