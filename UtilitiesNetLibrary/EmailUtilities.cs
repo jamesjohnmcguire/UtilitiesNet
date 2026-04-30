04a2a8838815eb80cec2df92b1d0d747018695ef
James John McGuire <jamesjohnmcguire@gmail.com>
2026-04-30 14:09:00 +0900

    Add EmailUtilities Class

---
+++ UtilitiesNetLibrary/EmailUtilities.cs
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////////////////
+// <copyright file="EmailUtilities.cs" company="Digital Zen Works">
+// Copyright © 2006 - 2026 Digital Zen Works.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.Common.Utilities;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class EmailUtilities
+{
+}
